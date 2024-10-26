fd3c946a398a40674babbe9eb5e4e72c180c75b6
masha240388@gmail.com <masha240388@gmail.com>
2024-10-27 00:48:09 +0300

    Test

---
+++ проба/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+//Правило : перевизначайте GetHeshCode первизначаючи Equals
+
+namespace ClassObject
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Object obj1 = new Object();
+            Object obj2 = new Object();
+            Console.WriteLine(obj1.Equals(obj2));
+        }
+    }
+    public class Point : object
+
+    {
+
+        protected int x, y;
+
+        public Point(int xValue, int yValue)
+
+        {
+            x = xValue;
+
+            y = yValue;
+
+        }
+
+        public override bool Equals(object obj)
+
+        {
+
+            if (obj == null || this.GetType() != obj.GetType())
+
+                return false;
+
+            Point p = (Point)obj;
+
+            return (x == p.x) && (y == p.y);
+
+        }
+
+       /* public override int GetHashCode()
+
+        {
+
+            return x ^ y;
+
+        }*/
+
+    }
+}
